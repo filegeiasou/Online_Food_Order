8ef746f8a808b4a239206ebf35fb4bbcc6da5439
Frosso Varsou <frossovarsou@gmail.com>
2024-08-14 20:35:12 +0300

    Minor Changes for VsCode

--- 4_5-Code-UAT/AppLogo.java
+++ 4_5-Code-UAT/AppLogo.java
@@ -5,7 +5,7 @@ public class AppLogo  {
     private ImageIcon icon;
     private JLabel imageLabel;
     AppLogo() {
-        icon = new ImageIcon("4_5-Code-UAT/logo.png");
+        icon = new ImageIcon("logo.png");
         icon = new ImageIcon(icon.getImage().getScaledInstance(100, 90, Image.SCALE_DEFAULT));
         imageLabel = new JLabel(icon);
         imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
